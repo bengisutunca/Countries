c9e263785f69b85e6fb5225704445b92a9c1dcac
Bengisu Tunca <bengitunca@gmail.com>
2022-10-10 17:29:23 +0300

    Added Saved Countries View Controller

---
+++ Countries/Controller/SavedCountriesViewController.swift
@@ -0,0 +1,58 @@
+//
+//  SavedCountriesViewController.swift
+//  Countries
+//
+//  Created by Bengi Tunca on 8.10.2022.
+//
+
+import UIKit
+
+final class SavedCountriesViewController: UIViewController {
+    
+    @IBOutlet private weak var savedCountriesCollectionView: UICollectionView!
+    
+    private var savedCountryNameList: [String?] = []
+    private var savedCountryCodeList: [String?] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        DispatchQueue.main.async {
+            self.savedCountryNameList = GlobalVariables.savedCountriesNameList
+            self.savedCountryCodeList = GlobalVariables.savedCountriesCodeList
+            self.savedCountriesCollectionView.reloadData()
+        }
+    }
+}
+
+extension SavedCountriesViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return savedCountryNameList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SavedCountriesCollectionViewCell", for: indexPath) as! SavedCountriesCollectionViewCell
+        cell.lblCountryName?.text = savedCountryNameList[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if let countryDetailsVC = storyboard?.instantiateViewController(withIdentifier: "DetailsViewController") as? DetailsViewController {
+            self.navigationController?.pushViewController(countryDetailsVC, animated: true)
+            countryDetailsVC.countryCode = savedCountryCodeList[indexPath.row]
+            countryDetailsVC.countryName = savedCountryNameList[indexPath.row]
+
+        }
+    }
+}
+
+extension SavedCountriesViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let height = view.frame.size.height
+        let width = view.frame.size.width
+        return CGSize(width: width * 0.9, height: height * 0.1)
+    }
+}
+
---
+++ Countries/View/Cells/SavedCountriesCollectionViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  SavedCountriesCollectionViewCell.swift
+//  Countries
+//
+//  Created by Bengi Tunca on 8.10.2022.
+//
+
+import UIKit
+
+final class SavedCountriesCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var lblCountryName: UILabel!
+    @IBOutlet private weak var btnFavorited: UIButton!
+    
+    override func awakeFromNib() {
+        makeRoundedCell()
+        super.awakeFromNib()
+    }
+    
+    var country: Country? {
+        didSet {
+            guard let country = country else {
+                return
+            }
+            if country.name == nil {
+                self.lblCountryName.text = GlobalVariables.noDataPlaceHolder
+            } else {
+                self.lblCountryName.text = country.name
+            }
+        }
+    }
+    
+    @IBAction func savedButtonTapped(_ sender: UIButton) {
+    }
+    
+    override func prepareForReuse() {
+        lblCountryName.text = nil
+        super.prepareForReuse()
+    }
+}
