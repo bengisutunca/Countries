c004d32ebfc815e15107a0e3bd3c580c32ac06f6
Bengisu Tunca <bengitunca@gmail.com>
2022-10-10 17:32:53 +0300

    Added Homepage Button Feature

--- Countries/View/Cells/CountryCollectionViewCell.swift
+++ Countries/View/Cells/CountryCollectionViewCell.swift
@@ -10,7 +10,7 @@ import UIKit
 final class CountryCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet private weak var lblCountryName: UILabel!
-    @IBOutlet private weak var btnAddFavorited: UIButton!
+    @IBOutlet weak var btnAddFavorited: UIButton!
     
     override func awakeFromNib() {
         makeRoundedCell()
@@ -23,11 +23,32 @@ final class CountryCollectionViewCell: UICollectionViewCell {
                 return
             }
             if country.name == nil {
-                self.lblCountryName.text = ""
+                self.lblCountryName.text = GlobalVariables.noDataPlaceHolder
             } else {
                 self.lblCountryName.text = country.name
             }
         }
+    }
+    
+    @IBAction func savedButtonTapped(_ sender: UIButton) {
+        if(GlobalVariables.savedCountriesCodeList.contains(self.country?.code ?? "")){
+            let deleteID = GlobalVariables.savedCountriesCodeList.firstIndex(where: {$0 == self.country?.code})!
+            let deleteName = GlobalVariables.savedCountriesNameList.firstIndex(where: {$0 == self.country?.name})!
+            
+            GlobalVariables.savedCountriesCodeList.remove(at: deleteID)
+            GlobalVariables.savedCountriesNameList.remove(at: deleteName)
+            
+            UserDefaults.standard.setValue(GlobalVariables.savedCountriesCodeList, forKey: UserDefaultsKey.countryCodesKey.rawValue)
+            btnAddFavorited?.setImage(UIImage(systemName: GlobalVariables.unsaved), for: .normal)
+            return
+        }
+        
+        GlobalVariables.savedCountriesCodeList.append(self.country?.code ?? "")
+        GlobalVariables.savedCountriesNameList.append(self.country?.name ?? "")
+        
+        UserDefaults.standard.set(GlobalVariables.savedCountriesNameList, forKey: UserDefaultsKey.countreyNamesKey.rawValue)
+        UserDefaults.standard.set(GlobalVariables.savedCountriesCodeList, forKey: UserDefaultsKey.countryCodesKey.rawValue)
+        btnAddFavorited?.setImage(UIImage(systemName: GlobalVariables.saved), for: .normal)
     }
     
     override func prepareForReuse() {
