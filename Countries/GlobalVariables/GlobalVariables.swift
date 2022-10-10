51fc9fb55a3d421dfca7679966b0a0835222ce59
Bengisu Tunca <bengitunca@gmail.com>
2022-10-10 17:30:12 +0300

    Added Global Variables

---
+++ Countries/GlobalVariables/GlobalVariables.swift
@@ -0,0 +1,28 @@
+//
+//  GlobalVariables.swift
+//  Countries
+//
+//  Created by Bengi Tunca on 8.10.2022.
+//
+
+import UIKit
+
+struct GlobalVariables {
+    // MARK: UserDefaults Arrays
+    static var savedCountriesCodeList = UserDefaults.standard.array(forKey: UserDefaultsKey.countryCodesKey.rawValue) as? [String] ?? []
+    static var savedCountriesNameList = UserDefaults.standard.array(forKey: UserDefaultsKey.countreyNamesKey.rawValue) as? [String] ?? []
+    
+    // MARK: Save Button Images file names
+    static var saved = "star.fill"
+    static var unsaved = "star"
+    
+    // MARK: Limit - Wiki Endpoint
+    static var limit: String = "10"
+    static var wiki = "https://www.wikidata.org/wiki/"
+    
+    // MARK: PlaceHolders
+    static var countryCodePlaceHolder = "Country Code:"
+    static var tabBarItemName1 = "Home"
+    static var tabBarItemName2 = "Saved"
+    static var noDataPlaceHolder = "There is nothing to show."
+}
---
+++ Countries/GlobalVariables/UserDefaultsKey.swift
@@ -0,0 +1,13 @@
+//
+//  UserDefaultsKey.swift
+//  Countries
+//
+//  Created by Bengi Tunca on 8.10.2022.
+//
+
+import Foundation
+
+enum UserDefaultsKey: String {
+    case countryCodesKey
+    case countreyNamesKey
+}
